c3f73f19fcb24c459856ce7fa0ca5ca032799b68
BraveScorpion <naction923@gmail.com>
2023-02-16 08:59:19 -0800

    101 qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints a string to a stderr using fprintf
+ * Return: 1 (successs)
+ */
+int main(void)
+{
+fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
