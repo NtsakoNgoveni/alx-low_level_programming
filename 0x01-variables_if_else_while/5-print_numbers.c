a86b16f37e98c853e7271514ce7df56f114fc031
BraveScorpion <naction923@gmail.com>
2023-02-17 05:40:02 -0800

    counting base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers of base starting 0
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (0 < 10)
+	{	printf("%d", n);
+	}	n++;
+	return (0);
+}
