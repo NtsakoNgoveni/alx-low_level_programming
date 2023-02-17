da3d7d94e73058e8080b0c5287c792c57b1fb62f
BraveScorpion <naction923@gmail.com>
2023-02-17 11:42:14 -0800

    triple nest

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main- prints combo of all possible combos of three single digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int one = 0, two = 1, tree = 2;
+
+	for (one = 0; one <= 7; one++)
+	{
+		for (; two <= 8; two++)
+		{
+			for (; tree <= 9; tree++)
+			{
+				if ((tree > two) && (two > one))
+				{
+					putchar(one + '0');
+					putchar(two + '0');
+					putchar(tree + '0');
+
+					if ((tree != 9) || (two != 8) || (one != 7))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
