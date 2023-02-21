bda0d23b46f3847e529cdf9041d8e5980d5a443f
BraveScorpion <naction923@gmail.com>
2023-02-21 09:34:52 -0800

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * time_table - prints the 9 times table
+ *Return:void
+ */
+void time_table(void)
+{
+	int k, i;
+
+	for (k = 0; k <= 9; k++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if ((i * k) > 10)
+			{
+				_putchar(i * k / 10 + '0');
+				_putchar(i * k % 10 + '0');
+			}
+			else
+				_putchar(i * k + '0');
+			if (i != 9 || k != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
