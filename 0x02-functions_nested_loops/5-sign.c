a68567535f6d33e6a5aa20d75197b1afc75d783f
BraveScorpion <naction923@gmail.com>
2023-02-21 05:40:50 -0800

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - determines wheter a number is negative or positive
+ * @a: is the number inspected
+ * Return: + if a> 0,- if a<0, 0 if a==a
+ */
+int print_sign(int a)
+{
+	if (a > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (a == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
