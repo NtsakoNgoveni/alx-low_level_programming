c3432d0bf219b366412505321ffe8eaf2bb843de
BraveScorpion <naction923@gmail.com>
2023-02-21 12:14:00 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - addns two intergers
+ * @a:num11 to be added to num 2
+ * @b:num2 to be added to num 1
+ * Return:returns sum of two intergers
+ */
+int add(int a, int b);
+{
+	return (a + b);
+}
