dfef175f55ddeb607d7250fc1b1bbbdcc4493c33
BraveScorpion <naction923@gmail.com>
2023-02-21 07:25:09 -0800

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,9 +15,9 @@ void jack_bauer(void)
 			_putchar(k / 10 + '0');
 			_putchar(k % 10 + '0');
 			_putchar(':');
-			_putchar(' ');
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
