89844e4aca38b29acafc764a005347e34c98bf93
BraveScorpion <naction923@gmail.com>
2023-03-08 03:17:02 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *strlen_recursion -calculate the length of a string
+ *@s: string to be calculated
+ *Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+int len = 0;
+
+if (*s)
+{
+	num++;
+	_strlen_recursion(s + 1);
+}
+return (len);
+}
