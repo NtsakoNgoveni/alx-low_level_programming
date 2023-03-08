b85d2916c99edea67e2d56d7a0705de50641b0da
BraveScorpion <naction923@gmail.com>
2023-03-08 05:03:05 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,9 +15,6 @@ return (1);
 else if (n % i == 0)
 return (0);
 
-else if (i == 0 || i < 0)
-return (0);
-
 else
 return (check_prime(n, i - 1));
 
@@ -33,6 +30,8 @@ int check_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
+if ( n <= 1)
+	return (0);
 return (check_prime(n, n - 1));
 }
 
