4bf5f8938a7d47717e394f9e179a608d7ed6acd7
BraveScorpion <naction923@gmail.com>
2023-03-13 16:41:30 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,7 +29,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (k = 0; av[i][k]; k++, j++)
+		for (k = 0, j = 0; av[i][k]; k++, j++)
 		ptr[j] = av[i][k];
 
 		if (ptr[j] == '\0')
