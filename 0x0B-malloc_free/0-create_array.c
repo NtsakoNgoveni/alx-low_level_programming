9e8022a759d8c4047fd66f8af139d33def2f2245
BraveScorpion <naction923@gmail.com>
2023-03-13 07:19:54 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array- creat an array on the heap and initializes it with a character
+ * @size:size of the array
+ * @c:the character used to initialiaze the array
+ * Return: pointer the the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * size + 1);
+
+	if (ptr == NULL || size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+In this project I will be applying my knowlegde of dynamic memory, malloc and free to solve task.
