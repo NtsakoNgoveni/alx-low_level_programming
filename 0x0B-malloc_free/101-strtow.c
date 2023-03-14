102996377f440a9875734eb86b9054ce8c92a1f7
BraveScorpion <naction923@gmail.com>
2023-03-14 06:45:34 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *strtow-splits words into string
+ *@str:string to be split
+ *Return:pointer to pointer to words
+ */
+
+char **strtow(char *str)
+{
+	int i, k, j = -1, a = 0, start, end, word = 0;
+	char **ptr;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i - 1] == ' ' || i == 0) && str[i] != ' ')
+			word++;
+	}
+	ptr = malloc(sizeof(char *) * (word + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i - 1] == ' ' || i == 0) && str[i] != ' ')
+		{
+			k = 0;
+			j++;
+			start = i;
+
+			while (str[i] != ' ' && str[i])
+			{
+				k++;
+				i++;
+			}
+			end = (i - 1);
+			ptr[j] = malloc(sizeof(char) * (k + 1));
+			if (ptr[j] == NULL)
+			{
+				for (i = 0; i < j; j++)
+					free(ptr[i]);
+				free(ptr);
+				return (NULL);
+			}
+			for (a = 0, i = start; i <= end; i++, a++)
+			{
+				ptr[j][a] = str[i];
+			}
+			ptr[j][a] = '\0';
+		}
+	}
+	ptr[j +1] = NULL;
+	return (ptr);
+}
