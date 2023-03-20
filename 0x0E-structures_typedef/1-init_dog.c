db893741ea150c7bbb21b550d8bd3dce9a065310
BraveScorpion <naction923@gmail.com>
2023-03-20 07:19:08 -0700

    task 1 and 2 typo fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,12 +9,10 @@
  * Return:void
  */
 
-void init_dog(struct dog_t *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog_t));
-	if (d == NULL)
-		return (NULL);
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,17 +7,16 @@
  * Return:void
  */
 
-void print_dog(struct dog_t *d)
+void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		prinf("(nil)");
-		return (NULL);
+		return;
 	}
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
