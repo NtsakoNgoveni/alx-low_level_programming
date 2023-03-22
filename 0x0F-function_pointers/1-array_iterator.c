99a475070e63b5fce007477ce88d4507a745adf5
BraveScorpion <naction923@gmail.com>
2023-03-22 12:04:29 -0700

    task 1 n task 2 fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * array_iterator-performs an action specifiedby a function on
  * all elements of the array
@@ -10,9 +11,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array == NULL || action == NULL || size == 0)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,6 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * int_index-searches for an inter in an array
  * @array:pointer to array that must be searched
@@ -16,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
+		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
