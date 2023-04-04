2e9c657b9d1ccc57d70b1cc0d5f1be1f3f4004f2
Ntsako Ngoveni <naction923@gmail.com>
2023-04-04 09:00:55 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(*head);
+		free(*ptr);
 		return (1);
 	}
 
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		ptr = ptr->next;
 
-		if (ptr == NULL)
+		if (ptr == NULL || ptr->next == NULL)
 		{
 			return (-1);
 		}
