5c29797874ac3c38cd4eb221a9da26f56ecd2c12
Ntsako Ngoveni <naction923@gmail.com>
2023-04-07 14:34:56 -0700

    fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,10 +7,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int zero = 0;
 	if (index > 63)
 		return (-1);
 
-	*n = ~(zero << index) & *n;
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
+
