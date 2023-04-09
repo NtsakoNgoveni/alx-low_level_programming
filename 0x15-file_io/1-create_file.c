81d97dabc73b4cfa4c845baa0fb219468bde8483
Ntsako Ngoveni <naction923@gmail.com>
2023-04-09 12:25:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	unsigned long int i = 0, len = 0;
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
-	if (!fd || fd == -1)
+	if (!filename || fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
