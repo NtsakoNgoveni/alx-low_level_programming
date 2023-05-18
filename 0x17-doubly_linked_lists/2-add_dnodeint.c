2daf74c805f0c82f0a1b523dae7be77252421601
BraveScorpion <naction923@gmail.com>
2023-05-18 04:51:45 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,7 +7,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(dlistint_t);
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
 	if (!new)
 		return (NULL);
