2b5b38efcedc5aaf732eed8e90e2c3a9739749ac
BraveScorpion <naction923@gmail.com>
2023-05-18 05:35:52 -0700

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a linked list
+ * @head: head of linked list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = head;
+
+	if (!head)
+		exit(1);
+
+	while (ptr)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+}
