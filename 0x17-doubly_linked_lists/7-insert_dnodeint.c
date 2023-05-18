5f94a27581bb2fb8034ecbb8a49d154ac599daf6
BraveScorpion <naction923@gmail.com>
2023-05-18 08:53:55 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,38 +8,37 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int i = 0;
-    dlistint_t *ptr = h ? *h : NULL, *ahead;
-    dlistint_t *new = malloc(sizeof(dlistint_t));
+unsigned int i = 0;
+dlistint_t *ptr = h ? *h : NULL, *ahead;
+dlistint_t *new = malloc(sizeof(dlistint_t));
 
-    if (!new || !h)
-        return (NULL);
-    new->n = n;
-    if (idx == 0)
-    {
-        new->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new;
-        *h = new;
-        new->prev = NULL;
-        return (new);
-    }
-    while (ptr && i < (idx - 1))
-    {
-        i++;
-        ptr = ptr->next;
-    }
-    if (!ptr || i != (idx - 1))
-    {
-        free(new);
-        return (NULL);
-    }
-    ahead = ptr->next;
-    ptr->next = new;
-    new->prev = ptr;
-    new->next = ahead;
-    if (ahead != NULL)
-        ahead->prev = new;
-    return (new);
+if (!new || !h)
+return (NULL);
+new->n = n;
+if (idx == 0)
+{
+new->next = *h;
+if (*h != NULL)
+(*h)->prev = new;
+*h = new;
+new->prev = NULL;
+return (new);
 }
-
+while (ptr && i < (idx - 1))
+{
+i++;
+ptr = ptr->next;
+}
+if (!ptr || i != (idx - 1))
+{
+free(new);
+return (NULL);
+}
+ahead = ptr->next;
+ptr->next = new;
+new->prev = ptr;
+new->next = ahead;
+if (ahead != NULL)
+	ahead->prev = new;
+return (new);
+}
