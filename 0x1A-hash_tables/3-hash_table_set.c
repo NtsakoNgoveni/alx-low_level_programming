e345470be380773c8a33e41a3217b3ec3abd4e3f
BraveScorpion <naction923@gmail.com>
2023-06-09 02:37:55 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,22 +13,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     hash_node_t *tmp;
     unsigned long int index;
 
-    new_item = malloc(sizeof(hash_node_t));
-    if (!new_item || !key || *key == '\0' || !ht || value == NULL)
+    if (!ht || !key || *key == '\0' || !value)
         return 0;
 
-	    index = key_index((const unsigned char *)key, ht->size);
+    new_item = malloc(sizeof(hash_node_t));
+    if (!new_item)
+        return 0;
+
     new_item->value = strdup(value);
-    new_item->next = NULL;
     new_item->key = strdup(key);
+    new_item->next = NULL;
 
     if (!new_item->value || !new_item->key)
     {
-	    free(new_item->value);
-	    free(new_item->key);
-	    free(new_item);
-	    return (0);
+        free(new_item->value);
+        free(new_item->key);
+        free(new_item);
+        return 0;
     }
+
+    index = key_index((const unsigned char *)key, ht->size);
 
     if (index >= ht->size)
     {
@@ -45,9 +49,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     else
     {
         tmp = ht->array[index];
-        new_item->next = tmp;
-	ht->array[index] = new_item;
+        while (tmp->next)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = new_item;
     }
 
     return 1;
 }
+
