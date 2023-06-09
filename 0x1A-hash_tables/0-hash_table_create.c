1bde3be7dfbc42d37121452b1c49dc1124d94753
BraveScorpion <naction923@gmail.com>
2023-06-09 08:38:38 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,9 +15,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size  = size;
-	new_table->array = malloc(sizeof(hash_table_t) * size);
+	new_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (new_table->array == NULL)
 	{
+		free(new_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
